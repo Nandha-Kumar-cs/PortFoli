6c7b3c5e3ec32c82328754a5b46dc3d7f846b31b
Nandhakumar <nandhakumararunachalam2003@gmail.com>
2025-04-30 17:15:03 +0530

    modfied

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,19 +2,22 @@ import "../About.css";
 export function About() {
   return (
     <>
-      <div className="about-container container" id="about-container">
-        <article>
+      <div
+        className="about-container container d-flex justify-content-center align-items-center"
+        id="about-container"
+      >
+        <article className="mx-2">
           <section>Hi, my name is</section>
           <section>Nandhakumar A.</section>
-          <section>PHP deep, frontend sweet.</section>
+          <section className="fw-semibold">PHP deep, frontend sweet.</section>
           <section>
-            “I'm a software engineer focused on scalable backend systems and
-            full-stack web development using PHP and modern technologies.”
+            I'm a software engineer specializing in backend development and
+            full-stack web applications using PHP and Laravel. I focus on
+            building scalable, secure systems that deliver user-centric digital
+            experiences.
           </section>
-          <section >
-           <button className="btn mt-md-5">
-           Discover My Work !
-           </button>
+          <section>
+            <button className="btn mt-md-5">Discover My Work !</button>
           </section>
         </article>
       </div>
--- src/components/FolioHeader.jsx
+++ src/components/FolioHeader.jsx
@@ -1,58 +1,162 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
+
 export function FolioHeader() {
   const [active, setActive] = useState("about");
 
   return (
     <>
+      <nav className="navbar navbar-expand-md header-navbar  mx-md-4 mt-md-2">
+        <div className="container-fluid">
+          <a className="navbar-brand " href="#">
+            <img src="../../public/n_icon_2.png" alt="" width={50} />
+          </a>
 
-        <nav className="navbar navbar-expand-lg header-navbar mx-md-5 mt-md-3">
-          <div className="container-fluid">
-            <a className="navbar-brand glitch-text " href="#">
-              NK
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse " id="navbarNav">
-              <ul className="navbar-nav ms-auto align-items-center">
+          <button
+            className="navbar-toggler d-md-none"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div
+            className="collapse navbar-collapse d-none d-md-flex"
+            id="navbarNav"
+          >
+            <ul className="navbar-nav ms-auto align-items-center justify-content-end">
+              {[
+                {
+                  id: "about",
+                  label: "About",
+                  no: "01.",
+                  link: "#about-container",
+                },
+                {
+                  id: "Experience",
+                  label: "Experience",
+                  no: "02.",
+                  link: "#about-container",
+                },
+                {
+                  id: "Work",
+                  label: "Work",
+                  no: "03.",
+                  link: "#about-container",
+                },
+                {
+                  id: "Contact",
+                  label: "Contact",
+                  no: "04.",
+                  link: "#about-container",
+                },
+              ].map((item) => (
+                <li key={item.id} className="nav-item ms-lg-3">
+                  <a
+                    href={item.link}
+                    className={`nav-link ${active === item.id ? "active" : ""}`}
+                    onClick={() => setActive(item.id)}
+                  >
+                    <span>{item.no}</span> {item.label}
+                  </a>
+                </li>
+              ))}
+              <li className="nav-item ms-lg-2 nav-item-resume">
+                <a className="nav-link" href="#">
+                  <button className="btn">Resume</button>
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Offcanvas Menu for Mobile (before md) */}
+          <div
+            className="offcanvas offcanvas-end d-md-none custom-offcanvas"
+            tabIndex="-1"
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+          >
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title d-none" id="offcanvasNavbarLabel">
+                Menu
+              </h5>
+              <div class="d-flex justify-content-end">
+                <button
+                  type="button"
+                  class="btn border-0"
+                  data-bs-dismiss="offcanvas"
+                  aria-label="Close"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="#64ffda"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      d="M2 2l12 12M2 14L14 2"
+                      stroke="#64ffda"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div className="offcanvas-body d-flex justify-content-center align-items-center text-center">
+              <ul className="navbar-nav d-flex ">
                 {[
-                  { id: "about", label: "About", no: "01" , link:"#about-container"},
-                  { id: "Experience", label: "Experience", no: "02",link:"#about-container" },
-                  { id: "Work", label: "Work", no: "03" ,link:"#about-container"},
-                  { id: "Contact", label: "Contact", no: "04",link:"#about-container" },
+                  {
+                    id: "about",
+                    label: "About",
+                    no: "01.",
+                    link: "#about-container",
+                  },
+                  {
+                    id: "Experience",
+                    label: "Experience",
+                    no: "02.",
+                    link: "#about-container",
+                  },
+                  {
+                    id: "Work",
+                    label: "Work",
+                    no: "03.",
+                    link: "#about-container",
+                  },
+                  {
+                    id: "Contact",
+                    label: "Contact",
+                    no: "04.",
+                    link: "#about-container",
+                  },
                 ].map((item) => (
-                  <li className="nav-item ms-lg-3">
+                  <li key={item.id} className="nav-item ">
                     <a
                       href={item.link}
-                      className={`nav-link ${
+                      className={`nav-link  fs-6${
                         active === item.id ? "active" : ""
                       }`}
                       onClick={() => setActive(item.id)}
                     >
-                      <span>{item.no}</span> {item.label}
+                      <span>{item.no}</span> <p>{item.label}</p>
                     </a>
                   </li>
                 ))}
-
-                <li className="nav-item  ms-lg-2 nav-item-resume">
+                <li className="nav-item nav-item-resume mt-3">
                   <a className="nav-link" href="#">
-                    <button className="btn">Resume</button>
+                    <button className="btn w-100">Resume</button>
                   </a>
                 </li>
               </ul>
             </div>
           </div>
-        </nav>
-   
+        </div>
+      </nav>
     </>
   );
 }
